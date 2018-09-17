4442342ae85ea4f985a9a01ac43ec660d521647b
arthiverma03 <arthiverma03@gmail.com>
2018-09-17 23:25:19 +0530

    yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,37 +1,24 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
-console.log(_.isString(true));
-console.log(_.isString('Aartiverma'));
 
-var filteredArray = _.uniq(['Aartiverma'])
-console.log(filteredArray);
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-console.log('Result:', notes.add(9, -2));
-
-var command = process.argv[2];
-console.log(process.argv);
-
-if (command ==='add'){
-    console.log('Adding all new notes');
-
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
 }
-else if(command ==='list'){
-    console.log('listing all notes')
-}
-else if(command === 'read'){
-    console.log('reading note');
-}
-else if (command === 'remove') {
-    console.log('remove');
-
-}
-else
-console.log('no command found');
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
