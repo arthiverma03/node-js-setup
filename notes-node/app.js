c6bd24d1d35c0bda11838bae31941670829cc7b6
arthiverma03 <arthiverma03@gmail.com>
2018-09-17 23:01:16 +0530

    process.argv

--- notes-node/app.js
+++ notes-node/app.js
@@ -12,6 +12,26 @@ console.log(filteredArray);
 
 console.log('Result:', notes.add(9, -2));
 
+var command = process.argv[2];
+console.log(process.argv);
+
+if (command ==='add'){
+    console.log('Adding all new notes');
+
+}
+else if(command ==='list'){
+    console.log('listing all notes')
+}
+else if(command === 'read'){
+    console.log('reading note');
+}
+else if (command === 'remove') {
+    console.log('remove');
+
+}
+else
+console.log('no command found');
+
 // var user = os.userInfo();
 //
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
